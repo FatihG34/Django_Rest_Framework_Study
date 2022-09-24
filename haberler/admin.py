f8ccffac178ac8172da872c6297390499e163097
FatihG34 <fthgnydn34@gmail.com>
2022-09-24 22:09:33 +0300

    Author model added

--- haberler/admin.py
+++ haberler/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from haberler.models import Makele
+from haberler.models import Author, Makele
 
 # Register your models here.
 admin.site.register(Makele)
+admin.site.register(Author)
--- haberler/models.py
+++ haberler/models.py
@@ -3,8 +3,17 @@ from django.db import models
 # Create your models here.
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    biography = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} {self.last_name}"
+
+
 class Makele(models.Model):
-    author = models.CharField(max_length=150)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="makaleler")
     title = models.CharField(max_length=120)
     description = models.CharField(max_length=200)
     content = models.TextField()
